6c9224cfe87ff6e0643ec9012eb1ff8620e87cd0
ayelenailin-m <pukyailin@gmail.com>
2024-05-03 16:42:54 -0300

    Cree el contenido de numAscendente.js

---
+++ assets/numAscendente.js
@@ -0,0 +1,22 @@
+let nro1 = parseInt(prompt("Ingrese el primer número entero"));
+let nro2 = parseInt(prompt("Ingrese un segundo número entero"));
+let nro3 = parseInt(prompt("Ingrese un tercer número entero"));
+
+if (!isNaN(nro1, nro2, nro3)) {
+    if (nro1 < nro2 && nro2 < nro3) {
+        console.log("El orden ascendente es: " + nro1 + ", " + nro2 + ", " + nro3);
+    } else if (nro3 < nro2 && nro2 < nro1){
+        console.log("El orden ascendente es: " + nro3 + ", " + nro2 + ", " + nro1);
+    } else if (nro2 < nro3 && nro3 < nro1){
+        console.log("El orden ascendente es: " + nro2 + ", " + nro3 + ", " + nro1);
+    } else if (nro3 < nro1 && nro1 < nro2){
+        console.log("El orden ascendente es: " + nro3 + ", " + nro1 + ", " + nro2);
+    } else if (nro1 < nro3 && nro3 < nro2){
+        console.log("El orden ascendente es: " + nro1 + ", " + nro3 + ", " + nro2);
+    } else if (nro2 < nro1 && nro1 < nro3){
+        console.log("El orden ascendente es: " + nro2 + ", " + nro1 + ", " + nro3);
+    } 
+} else {
+    console.log("No es un número entero válido");
+}
+
